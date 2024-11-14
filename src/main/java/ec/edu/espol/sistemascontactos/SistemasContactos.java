100fe6938458298fcd8da22a4e8b17e78792383a
Juseperez <juseperez@espol.edu.ec>
2024-11-14 17:44:21 -0500

    PROYECTO

---
+++ src/main/java/ec/edu/espol/sistemascontactos/SistemasContactos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.sistemascontactos;
+
+/**
+ *
+ * @author DR JUAN PEREZ
+ */
+public class SistemasContactos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
